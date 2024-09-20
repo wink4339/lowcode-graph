b2c3661583e194fa48323b60ca3ed8ec51f10993
wink4339 <wink394339@gmail.com>
2024-09-20 08:37:44 +0800

    chore: 实体索引拖曳

--- packages/plugin-x6-er-indexes/src/dialog/index.tsx
+++ packages/plugin-x6-er-indexes/src/dialog/index.tsx
@@ -2,7 +2,8 @@ import { createContext, useContext, useEffect, useState, useRef } from 'react'
 import { Icon, Dialog, Checkbox, Balloon } from '@alifd/next'
 import { event } from '@alilc/lowcode-engine';
 import './index.scss'
-import { DiaogType, IDiaogOptions } from '../types';
+import { IDiaogOptions } from '../types';
+import { v4 as uuid } from 'uuid';
 
 const DialogContext = createContext({
   openDialog: (options: IDiaogOptions = {}) => {
@@ -29,32 +30,17 @@ export default function InlineDialog() {
   const [options, setOptions] = useState({} as IDiaogOptions)
   const [loading, setLoading] = useState(false)
 
-  const [fields, setFields] = useState([
-    {
-      id: 'id',
-      name: 'ID',
-      disabled: true
-    },
-    {
-      id: 'create_time',
-      name: '创建时间'
-    },
-    {
-      id: 'update_time',
-      name: '更新时间'
-    },
-    {
-      id: 'order_sn',
-      name: '入库单编号'
-    }
-  ])
+  const [columns, setColumns] = useState([] as any)
+  const [columnsMap, setColumnsMap] = useState({} as any)
 
   const [fieldsRef, setFieldsRef] = useState({} as any)
 
   const [indexes, setIndexes] = useState([] as any)
   const [selectedIndex, setSelectedIndex] = useState('')
+  const [selected, setSelected] = useState('')
 
   useEffect(() => {
+    init()
     event.on(`common:generic.openDialog`, openDialog)
     event.on(`common:generic.closeDialog`, closeDialog)
     event.on(`common:generic.openDialogLoading`, openDialogLoading)
@@ -67,6 +53,50 @@ export default function InlineDialog() {
       event.off('common:generic.closeDialogLoading', closeDialogLoading)
     }
   }, [])
+
+  const init = () => {
+    const columns = [
+      {
+        columnId: "id",
+        columnName: "ID"
+      },
+      {
+        columnId: "username",
+        columnName: "用户名"
+      },
+      {
+        columnId: "orderId",
+        columnName: "订单id"
+      },
+      {
+        columnId: "tel",
+        columnName: "手机号"
+      },
+      {
+        columnId: "create_time",
+        columnName: "创建时间"
+      },
+      {
+        columnId: "update_time",
+        columnName: "更新时间"
+      }
+    ]
+    let columnsMap = {} as any
+    for(let i in columns) {
+      columnsMap[columns[i].columnId] = i
+    }
+    
+    setColumns(columns)
+    setColumnsMap(columnsMap)
+
+    // setIndexes([
+    //   {
+    //     id: "",
+    //     name: "",
+    //     fields: []
+    //   }
+    // ])
+  }
 
 
   const openDialog = (options: IDiaogOptions = {}) => {
@@ -93,277 +123,223 @@ export default function InlineDialog() {
     }
   }
 
-  const generateCtxKey = (sign: any) => {
-    return `${new Date().getTime()}-${sign}`
-  }
-
   const addIndex = () => {
-    const size = indexes.length
-    const key = generateCtxKey(size)
+    const key = uuid()
     const index = {
       key,
+      id: '',
       name: '',
-      fields: ''
+      fields: []
     } as any
     setIndexes(indexes.concat(index))
-    setSelectedIndex(key)
+    setSelected(key)
+  }
+
+  const findColumn = (columnId: string) => {
+    if (!columnId) return null
+    for(let i=0; i<columns.length; i++) {
+      if (columns[i].columnId == columnId) {
+        return {i, ...columns[i]}
+      }
+    }
+    return null
+  }
+
+  const findIndex = (key: string) => {
+    if (!key) return null
+    for(let i=0; i<indexes.length; i++) {
+      if (indexes[i].key == key) {
+        return {i, ...indexes[i]}
+      }
+    }
+    return null
+  }
+
+  const findIndexField = (key: string, id: string) => {
+    if (!key) return null
+    const index = findIndex(key)
+    for(let i=0; i<index.fields.length; i++) {
+      const field = index.fields[i]
+      if(field.id == id) {
+        return {i, ...field}
+      }
+    }
+    return null
+  }
+
+  const generateIndexName = (index: any) => {
+    let name = ''
+    for(let i=0; i<index.fields.length; i++) {
+      if(i !=0 ) name += '-'
+      name += index.fields[i].id
+    }
+    return name
   }
 
   const getDisplayName = (k: string) => {
-    const indexFields = getIndexFields(k)
-
+    const index = findIndex(k)
     let displayName = ''
-    for(const index of indexFields) {
-      for(const field of fields) {
-        if (index.field == field.id) {
-          if (displayName) {
-            displayName += `-`
-          }
-          displayName += field.name
-        }
+
+    for(let i=0; i<index.fields.length; i++) {
+      const item = index.fields[i]
+      const column = findColumn(item.id)
+      if (column) {
+        if (i != 0) displayName += '-'
+        displayName += column.columnName
       }
     }
     return displayName
   }
 
   const getDisplayLabel = () => {
-    const indexFields = getIndexFields(selectedIndex) as any[]
-    const size = indexFields.length
+    const index = findIndex(selected)
+    const size = index.fields.length
     if (size > 0) {
       return `已选择 ${size} 个字段`
     } 
     return '请选择索引字段'
   }
 
-  const getIndexFields = (k: string) => {
-    if (!k) return []
-    if (indexes.length == 0) return []
-
-    const index = findIndex(k)
-    if (!index || !index.fields) return []
-
-    return index.fields.split(',').map((e: any) => {
-      const ss = e.trim().split(' ')
-      return { field: ss[0], sortOrder: ss[1]}
-    })
-  }
-
-  const findIndex = (k: string) => {
-    for (let index of indexes) {
-      if (index.key == k) {
-        return index
-      }
-    }
-    return undefined
-  }
-
-  const selectedFields = () => {
-    const indexFields = getIndexFields(selectedIndex)
-    const res = []
-    for(const index of indexFields) {
-      for(let field of fields) {
-        if (index.field == field.id) {
-          // @ts-ignore
-          field.sortOrder = index.sortOrder
-          res.push(field)
-          break 
-        }
+  const selectedColumns = () => {
+    if (!selected) return []
+    let res = []
+    for(let i=0; i<columns.length; i++) {
+      let item = columns[i]
+      const field = findIndexField(selected, item.columnId)
+      if (field) {
+        res.push({...item, sortOrder: field.sortOrder})
       }
     }
     return res
   }
-
-  const unselectedFields = () => {
-    const indexFields = getIndexFields(selectedIndex)
-    return fields.filter(e => !indexFields.some((f: any) => e.id == f.field))
+  const unselectedColumns = () => {
+    if (!selected) return columns
+    return columns.filter((e: any) => !findIndexField(selected, e.columnId))
   }
 
-  const handleUnselectedFieldChange = (field: any, state: boolean) => {
-    if (!state) return
+  const handleUnselectedFieldChange = (columnId: any) => {
+    let index = findIndex(selected)
+    if (!index) return
 
-    let index = findIndex(selectedIndex)
-    if (!index) index = {}
-
-    if (index.fields) {
-      index.name += '-'
-      index.fields += ','
-    } 
-    index.name += field.id,
-    index.fields += `${field.id} ASC`
+    index.fields.push({id: columnId, sortOrder: 'ASC'})
+    index.name = generateIndexName(index)
+    indexes[index.i] = index
     setIndexes(JSON.parse(JSON.stringify(indexes)))
   }
 
-  const handleSelectedFieldChange = (id: string, state: boolean) => {
-    if (state) return
-
-    let indexFields = getIndexFields(selectedIndex).filter((e: any) => e.field != id)
-    let index = findIndex(selectedIndex)
-    index.name = ''
-    index.fields = ''
-    indexFields.forEach((e: any) => {
-      if (index.fields) {
-        index.name += '-'
-        index.fields += ','
-      } 
-      index.name += e.field,
-      index.fields += `${e.field} ${e.sortOrder}`
-    });
-
-    for(let i in indexes) {
-      if (indexes[i].key == selectedIndex) {
-        indexes[i] == index
-        break
-      }
-    }
-    setIndexes(JSON.parse(JSON.stringify(indexes)))
+  const handleSelectedFieldChange = (id: string) => {
+    let index = findIndex(selected)
+    let field = findIndexField(selected, id)
+    index.fields.splice(field.i, 1)
+    resetSelected()
   }
 
   const handleSelectedFieldSortOrderChange = (id: string, sortOrder: string) => {
-    let indexFields = getIndexFields(selectedIndex)
-    for(let i in indexFields) {
-      if (indexFields[i].field == id) {
-        indexFields[i].sortOrder = sortOrder
-        break
-      }
-    }
-    let index = findIndex(selectedIndex)
-    index.name = ''
-    index.fields = ''
-    indexFields.forEach((e: any) => {
-      if (index.fields) {
-        index.name += '-'
-        index.fields += ','
-      } 
-      index.name += e.field,
-      index.fields += `${e.field} ${e.sortOrder}`
-    });
-
-    for(let i in indexes) {
-      if (indexes[i].key == selectedIndex) {
-        indexes[i] == index
-        break
-      }
-    }
+    const index = findIndex(selected)
+    const field = findIndexField(index.key, id)
+    field.sortOrder = sortOrder
+    index.fields[field.i] = field
+    indexes[index.i] = index
     setIndexes(JSON.parse(JSON.stringify(indexes)))
   }
 
 
   const handleSelectIndex = (k: string) => {
-    setSelectedIndex(k)
+    setSelected(k)
   }
 
   const handleRemoveIndex = (k: string) => {
-    for (let i in indexes) {
-      if (indexes[i].key == k) {
-        indexes.splice(i, 1)
-        setIndexes(JSON.parse(JSON.stringify(indexes)))
-        if (k == selectedIndex) {
-          setSelectedIndex('')
-        }
-        return
-      }
-    }
+    const index = findIndex(k)
+    indexes.splice(index.i, 1)
+    if (selected == k) setSelected('')
+    setIndexes(JSON.parse(JSON.stringify(indexes)))
   }
 
-  let elementPosition = 0
-  let elementTop = 0
+  let eleId: any
+  let eleTop = 0
   let offsetY = 0
   let dragging = false
   let maxTop = 0
   let moveFields = [] as any[]
 
-  const getSelectedFieldPositon = (id: string) => {
-    const fields = selectedFields() as any[]
-    for(let i in fields) {
-      if (fields[i].id == id)
-        return Number.parseInt(i)
-    }
-    return 0
-  }
-
-  const handleMouseDown = (id: string, event: any) => {
+  const handleMouseDown = (id: any, event: any) => {
     if (dragging) return
 
-    moveFields = selectedFields()
-    const size = moveFields.length
-    if (size < 2) return
+    const index = findIndex(selected)
+    if(index.fields.length < 2) return
 
-    const element = refs.current[id];
-    const lastElement = refs.current[moveFields[size - 1].id];
+    const ele = document.getElementById(id) as HTMLElement 
+    const lastEle = document.getElementById(index.fields[index.fields.length - 1].id) as HTMLElement;
+    eleTop = Number.parseInt(ele.style.top.replace('px', ''))
+    maxTop = Number.parseInt(lastEle.style.top.replace('px', ''))
 
-    elementPosition = getSelectedFieldPositon(id)
-    elementTop = Number.parseInt(element.style.top.replace('px', ''))
-    maxTop = Number.parseInt(lastElement.style.top.replace('px', ''))
-    offsetY = event.clientY;
+    offsetY = event.clientY
+    eleId = id
     dragging = true
   };
 
-  const handleMouseMove = (id: string, event: any) => {
+  const handleMouseMove = (event: any) => {
     if (!dragging) return
-    const element = refs.current[id];
+    const ele = document.getElementById(eleId) as HTMLElement 
     const newY = event.clientY - offsetY;
-    const top = elementTop + newY
-    if (top < 0) {
-      element.style.top = `${0}px`
-    } else if (top > maxTop) {
-      element.style.top = `${maxTop}px`
-    } else {
-      element.style.top = `${top}px`
-    }
-    element.style.zIndex = 999
-    const position = Math.round(top / 45.0)
-    
-    if (elementPosition == position) return
+    let top = eleTop + newY
+    if (top < 0) top = 1
+    if (top > maxTop) top = maxTop
+   
+    ele.style.top = `${top}px`
+    ele.style.zIndex = '999'
 
-    let tmp = moveFields[elementPosition]
-    if (position < elementPosition) {
-      for (let i = elementPosition; i > position; i--) {
-          moveFields[i] = moveFields[i - 1]
-          refs.current[moveFields[i].id].style.top = `${i*45}px`
+    const index = findIndex(selected)
+    const fields = index.fields
+
+    const position = Math.round(top / 45.0)
+    const fPositin = findIndexField(selected, eleId).i
+    if ( position == fPositin) return
+
+    let tmp = fields[fPositin]
+    if (position < fPositin) {
+      for (let i = fPositin; i > position; i--) {
+        fields[i] = fields[i - 1]
+        const ele = document.getElementById(fields[i].id) as HTMLElement 
+        ele.style.top = `${i*45}px`
+        ele.style.marginTop = `${i == 0 ? 0 : -1}px`
       } 
-    } 
-    if (position > elementPosition) {
-      for(let i=elementPosition; i < position; i++) {
-        moveFields[i] = moveFields[i + 1]
-        refs.current[moveFields[i].id].style.top = `${i*45}px`
+    }
+    if (position > fPositin) {
+      for(let i=fPositin; i < position; i++) {
+        fields[i] = fields[i + 1]
+        const ele = document.getElementById(fields[i].id) as HTMLElement 
+        ele.style.top = `${i*45}px`
+        ele.style.marginTop = `${i == 0 ? 0 : -1}px`
       }
     }
-    moveFields[position] = tmp
-    elementPosition = position
+    fields[position] = tmp
   }
 
-  const handleMouseUp = (id: string, event: any) => {
-    if (!dragging) return
+  const handleMouseUp = (event: any) => {
     dragging = false
-    const element = refs.current[id];
-    element.style.zIndex = 1
-    refs.current[id].style.top = `${elementPosition*45}px`
-
-    // 最后更新有问题
-    // let index = {name : '', fields: }
-    // index.name = ''
-    // index.fields = ''
-    // indexFields.forEach((e: any) => {
-    //   if (index.fields) {
-    //     index.name += '-'
-    //     index.fields += ','
-    //   } 
-    //   index.name += e.field,
-    //   index.fields += `${e.field} ${e.sortOrder}`
-    // });
-
-    // for(let i in indexes) {
-    //   if (indexes[i].key == selectedIndex) {
-    //     indexes[i] == index
-    //     break
-    //   }
-    // }
-    // setIndexes(JSON.parse(JSON.stringify(indexes)))
+    resetSelected()
   }
 
-  const size = getIndexFields(selectedIndex).length
+  const resetSelected = () => {
+    let index = findIndex(selected)
+    const fields = index.fields
+    for(let i=0; i<fields.length; i++) {
+      const ele = document.getElementById(fields[i].id) as HTMLElement 
+      ele.style.top = `${i*45}px`
+      ele.style.marginTop = `${i == 0 ? 0 : -1}px`
+      ele.style.zIndex = '0'
+    }
+    index.name = generateIndexName(index)
+    indexes[index.i] = index
+    setIndexes(JSON.parse(JSON.stringify(indexes)))
+  }
+
+  const size = selectedColumns().length
+  console.log("高度", size)
   const height = 46
   const dragHeight = size * (height - 1)
+  console.log("refs.current", refs)
   return (
     <Dialog
       v2  
@@ -380,7 +356,7 @@ export default function InlineDialog() {
             <div className='field-menu'>
               <div className='field-menu-scroll'>
                 {
-                  indexes.map((e: any, i: number) => <div key={e.key} className={`fields-menu-item ${selectedIndex == e.key ? 'active': ''}`}>
+                  indexes.map((e: any, i: number) => <div key={e.key} className={`fields-menu-item ${selected == e.key ? 'active': ''}`}>
                      <Balloon v2 closable={false} trigger={<p onClick={() => handleSelectIndex(e.key)}>{i + 1}: { getDisplayName(e.key) || '空'}</p>}>
                         {i + 1}: { getDisplayName(e.key) || '空'}
                      </Balloon>
@@ -394,7 +370,7 @@ export default function InlineDialog() {
               <div className="fields-add-button" onClick={addIndex}>+新增索引</div>
             </div>
             {
-              !indexes.length || !selectedIndex
+              !indexes.length || !selected
               ?
               <div className="fields-content fields-empty">
                 <img src="https://img.alicdn.com/imgextra/i3/O1CN011k9Ijg1pi2e5xFRfj_!!6000000005393-2-tps-76-74.png" width="38" height="37"/>请创建索引
@@ -408,23 +384,25 @@ export default function InlineDialog() {
                     size == 0 ? null :
                     <div className='base-component-sort-list' style={{height: dragHeight}}>
                       {
-                        selectedFields().map((e: any, i) => 
-                          <div key={e.id} ref={el => refs.current[e.id] = el} 
+                        selectedColumns().map((e: any, i) => 
+                          <div
+                            id={e.columnId} 
+                            key={e.columnId}
                             className='base-component-sort-list-item fields-item' 
                             style={{height, top: i * (height - 1)}}
-                            onMouseMove={(event) => handleMouseMove(e.id, event)}
-                            onMouseUp={(event) => handleMouseUp(e.id, event)}
+                            onMouseMove={handleMouseMove}
+                            onMouseUp={handleMouseUp}
                           >
                             <span className='base-component-sort-list-item-drag' style={{height}} 
-                              onMouseDown={(event) => handleMouseDown(e.id, event)}
+                              onMouseDown={(event) => handleMouseDown(e.columnId, event)}
                             />
                             <div className='base-component-sort-list-item-content'>
-                              <Checkbox style={{margin: '0px 16px'}} defaultChecked onChange={(state) => handleSelectedFieldChange(e.id, state)}/>{e.name}
+                              <Checkbox style={{margin: '0px 16px'}} defaultChecked onChange={() => handleSelectedFieldChange(e.columnId)}/>{e.columnName}
                             </div>
                             <span className='base-component-sort-list-item-action'>
                                 <div className="item-order-action">
-                                  <span className={e.sortOrder == 'ASC' ? 'active' : ''} onClick={() => handleSelectedFieldSortOrderChange(e.id, 'ASC')}>升序</span>
-                                  <span className={e.sortOrder == 'DESC' ? 'active' : ''} onClick={() => handleSelectedFieldSortOrderChange(e.id, 'DESC')}>降序</span>
+                                  <span className={e.sortOrder == 'ASC' ? 'active' : ''} onClick={() => handleSelectedFieldSortOrderChange(e.columnId, 'ASC')}>升序</span>
+                                  <span className={e.sortOrder == 'DESC' ? 'active' : ''} onClick={() => handleSelectedFieldSortOrderChange(e.columnId, 'DESC')}>降序</span>
                                 </div>
                             </span>
                           </div>
@@ -434,9 +412,9 @@ export default function InlineDialog() {
                   }
                   <div style={{marginTop: size > 0 ? '-1px': '0px'}}>
                       {
-                        unselectedFields().map(e => 
-                          <div key={e.id} className='fields-item'>
-                            <Checkbox style={{margin: '0px 16px 0px 53px'}} disabled={e.disabled} onChange={(state) => handleUnselectedFieldChange(e, state)}/>{e.name}
+                        unselectedColumns().map((e: any) => 
+                          <div key={e.columnId} className='fields-item'>
+                            <Checkbox style={{margin: '0px 16px 0px 53px'}} disabled={e.disabled} onChange={() => handleUnselectedFieldChange(e.columnId)}/>{e.columnName}
                           </div>
                         )
                       }
