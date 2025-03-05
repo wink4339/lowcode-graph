934e5a36e72126f7b2d3f9b241147980ca2fdda7
wink4339 <wink394339@gmail.com>
2025-03-05 16:49:32 +0800

    chore: plugin-core移除监听文档

--- packages/plugin-core/src/index.ts
+++ packages/plugin-core/src/index.ts
@@ -1,44 +1,22 @@
-import { IPublicModelPluginContext, IPublicEnumTransformStage } from '@alilc/lowcode-types'
+import { IPublicModelPluginContext } from '@alilc/lowcode-types'
 import { AssetLoader } from '@alilc/lowcode-utils';
 import Inject, { injectAssets } from '@alilc/lowcode-plugin-inject';
-import { debounce } from 'lodash'
 
 const PluginCore = (ctx: IPublicModelPluginContext, options: any) => {
   return {
     async init() {
-      const { material, project, plugins, event } = ctx;
-      const { resourceId, resourceName, debounceDuration, assets, schema } = options
-
-      const debouncedHandleChange = debounce(() => {
-        const newSchema = JSON.stringify(project.exportSchema(IPublicEnumTransformStage.Save).componentsTree[0])
-        event.emit('resource.schemaChange', {resourceId, resourceName, schema: newSchema})
-      }, debounceDuration || 1000);
+      const { material, project, plugins } = ctx;
+      const { assets, schema } = options
 
       // debug注册
       await plugins.register(Inject);
+
       // 设置物料描述
       await material.setAssets(await injectAssets(assets));
 
       // 加载 schema
       project.openDocument(schema);
-
-      // 监听文档内容变更
-      project.currentDocument?.onAddNode(() => {
-        debouncedHandleChange()
-      })
-      project.currentDocument?.onChangeNodeProp(() => {
-        debouncedHandleChange()
-      })
-      project.currentDocument?.onChangeNodeChildren(() => {
-        debouncedHandleChange()
-      })
-      project.currentDocument?.onDropLocationChanged(() => {
-        debouncedHandleChange()
-      })
-      project.currentDocument?.onRemoveNode(() => {
-        debouncedHandleChange()
-      })
-
+      
       // 简单处理 init 时候直接 load 所有组件 view
       const loader = new AssetLoader();
       const componentsAssets = assets.packages.map((asset: any) => asset.urls).flat();
